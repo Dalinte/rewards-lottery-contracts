7dcbaff3fe4a826bb40a2f4aeedff79897f0967d
Sergey <savchuk-sergej@smartworld.team>
2022-08-30 18:28:07 +0700

    create ticket contract

---
+++ contracts/ticket.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Ticket is ERC20, Ownable  {
+    constructor(string memory _name, string memory _symbol, uint256 _initSupple) ERC20(_name, _symbol) Ownable() {
+        _mint(msg.sender, _initSupple);
+    }
+
+     function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+
+    function mint (address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
